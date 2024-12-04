2fb8f095acd6c4916cdc0f5662d13c884940ea1e
RomanGrishin <roman1988grishin@gmail.com>
2024-12-04 10:31:21 +0200

    Add projects to git

---
+++ BackupConfigManager/BackupManagerApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿namespace BackupConfigManager
+{
+    public sealed class BackupManagerApplicationContext : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+
+        public BackupManagerApplicationContext()
+        {
+            var menus = new ContextMenuStrip();
+            menus.Items.Add("Exit", null, Exit);
+            _trayIcon = new NotifyIcon
+            {
+                Icon = new Icon("book.ico"),
+                ContextMenuStrip = menus,
+                Visible = true
+            };
+        }
+
+        private void Exit(object? sender, EventArgs e)
+        {
+            _trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
---
+++ BackupDaemon/Program.cs
@@ -0,0 +1,7 @@
+using BackupDaemon;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
